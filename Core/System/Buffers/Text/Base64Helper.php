fd58cd05a6132350ac84379123a6dc849b8cb036
Christophe@chamborelle.local <christophe@chamborelle.fr>
2024-11-06 17:33:42 +0100

    INFORMATIQUE-christophe (06 nov. 2024 17:33)

---
+++ Core/System/Buffers/Text/Base64Helper.php
@@ -0,0 +1,44 @@
+﻿<?php
+
+/**
+ * Copyright since 2024 Firstruner and Contributors
+ * Firstruner is an Registered Trademark & Property of Christophe BOULAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Freemium License
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to contact@firstruner.fr so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit, reproduce ou modify this file.
+ * Please refer to https://firstruner.fr/ or contact Firstruner for more information.
+ *
+ * @author    Firstruner and Contributors <contact@firstruner.fr>
+ * @copyright Since 2024 Firstruner and Contributors
+ * @license   Proprietary
+ * @version 2.0.0
+ */
+
+
+namespace System\Buffers\Text;
+
+class Base64Helper
+{
+      public static function is_base64($s) : bool
+      {
+            // Check if there are valid base64 characters
+            if (!preg_match('/^[a-zA-Z0-9\/\r\n+]*={0,2}$/', $s)) return false;
+        
+            // Decode the string in strict mode and check the results
+            $decoded = base64_decode($s, true);
+            if(false === $decoded) return false;
+        
+            // Encode the string again
+            if(base64_encode($decoded) != $s) return false;
+        
+            return true;
+        }
+}
